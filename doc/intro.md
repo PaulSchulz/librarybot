69c6de312ee0554263aebdfc7b1a2cad85b40a43
Paul Schulz <paul@mawsonlakes.org>
2019-07-12 20:00:16 +0930

    Initial commit
    The initial software has been configured as a 'clojure' software
    project. It may end up being more Python code though.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to junkbot
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/junkbot/core_test.clj
@@ -0,0 +1,7 @@
+(ns junkbot.core-test
+  (:require [clojure.test :refer :all]
+            [junkbot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
