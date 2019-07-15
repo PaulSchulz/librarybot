3b71454f0ab680a0ea6825908b5f15008c866f01
Paul Schulz <paul@mawsonlakes.org>
2019-07-15 15:45:20 +0930

    Modification on servo-test

--- python/servo-test.py
+++ python/servo-test.py
@@ -1,9 +1,22 @@
 import board
- 
+import busio
+import adafruit_pca9685 # Allows more control over PWM
+
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
+
 print("Hello blinka!")
  
 # Try to create an I2C device
-i2c = busio.I2C(board.SCL, board.SDA)
-print("I2C ok!")
- 
+#i2c = busio.I2C(board.SCL, board.SDA)
+#hat = adafruit_pca9685.PCA9685(i2c)
+#print("I2C ok!")
+
+servo0 = 8
+servo1 = 9
+
+kit.servo[servo1].angle = 180
+time.sleep(1)
+kit.servo[servo1].angle = 0
+
 print("done!")
