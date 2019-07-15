b17d24b88155079fa2b5c42c27d378e8202abf02
Paul Schulz <paul@mawsonlakes.org>
2019-07-15 16:18:12 +0930

    Adds for servo tests

--- python/servo-test.py
+++ python/servo-test.py
@@ -1,6 +1,7 @@
+import time
 import board
 import busio
-import adafruit_pca9685 # Allows more control over PWM
+# import adafruit_pca9685 # Allows more control over PWM
 
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
@@ -12,11 +13,45 @@ print("Hello blinka!")
 #hat = adafruit_pca9685.PCA9685(i2c)
 #print("I2C ok!")
 
-servo0 = 8
-servo1 = 9
+motorLeft  = 0
+motorRight = 1
 
-kit.servo[servo1].angle = 180
+microservo0 = 2
+microservo1 = 3
+
+# Test servos
+print("Test microservo0")
+kit.servo[servo0].angle = 90
+time.sleep(1)
+kit.servo[servo0].angle = 0
+time.sleep(1)
+
+print("Test microserver1")
+kit.servo[servo1].angle = 90
 time.sleep(1)
 kit.servo[servo1].angle = 0
+time.sleep(1)
+
+print("Test Motor Left - forward")
+kit.continuous_servo[motorLeft].throttle = 1
+time.sleep(1)
+kit.continuous_servo[motorLeft].throttle = 0
+time.sleep(1)
+print("Test Motor Left - backwards")
+kit.continuous_servo[motorLeft].throttle = -1
+time.sleep(1)
+kit.continuous_servo[motorLeft].throttle = 0
+time.sleep(1)
+
+print("Test Motor Right - forward")
+kit.continuous_servo[motorRight].throttle = 1
+time.sleep(1)
+kit.continuous_servo[motorRight].throttle = 0
+time.sleep(1)
+print("Test Motor Right - backwards")
+kit.continuous_servo[motorRight].throttle = -1
+time.sleep(1)
+kit.continuous_servo[motorRight].throttle = 0
+time.sleep(1)
 
 print("done!")
