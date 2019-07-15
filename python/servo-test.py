5a03a0022187f08155b429acd0355e79edc4afd4
Paul Schulz <paul@mawsonlakes.org>
2019-07-15 15:24:56 +0930

    Addes servo-test script

---
+++ python/servo-test.py
@@ -0,0 +1,11 @@
+import board
+import digitalio
+import busio
+ 
+print("Hello blinka!")
+ 
+# Try to create an I2C device
+i2c = busio.I2C(board.SCL, board.SDA)
+print("I2C ok!")
+ 
+print("done!")
